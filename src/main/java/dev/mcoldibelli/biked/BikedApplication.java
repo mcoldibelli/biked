814ceb013cf8300a4fbed044ef814652778151c5
Marcelo Coldibelli <marcelo.coldibelli@gmail.com>
2025-12-09 21:42:37 -0300

    Initializa project

---
+++ src/main/java/dev/mcoldibelli/biked/BikedApplication.java
@@ -0,0 +1,13 @@
+package dev.mcoldibelli.biked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mcoldibelli/biked/BikedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mcoldibelli.biked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
